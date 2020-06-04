a8242bc3c4aa41fe7b5f44d72c79d797c47f6472
badal singh <prithvis811@gmail.com>
2020-06-04 21:24:55 +0530

    second

--- pro.java
+++ pro.java
@@ -3,11 +3,20 @@ public class pro
 public static void main(String []args)
 {
 int fulltime=1;
+int emprate=20;
+int emphrs=0;
+int empwage=0;
 System.out.println("welcome to employee wage computation");
 double emp=Math.floor(Math.random() * 10 ) % 2;
 if (emp==fulltime)
+      {
+	emphrs=8;
 	System.out.println("employee is present");
+      }
 else
         System.out.println("employee is absent");
+	
+    empwage=emphrs*emprate;
+   System.out.println("wage :" + empwage);
 }
 }

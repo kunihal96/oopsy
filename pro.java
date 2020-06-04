bd5ee24b2fbf9cf42decd2b883306d50ef714a37
badal singh <prithvis811@gmail.com>
2020-06-04 21:23:15 +0530

    frst

--- pro.java
+++ pro.java
@@ -2,6 +2,12 @@ public class pro
 {
 public static void main(String []args)
 {
-System.out.println("welcome to assignment 23");
+int fulltime=1;
+System.out.println("welcome to employee wage computation");
+double emp=Math.floor(Math.random() * 10 ) % 2;
+if (emp==fulltime)
+	System.out.println("employee is present");
+else
+        System.out.println("employee is absent");
 }
 }
